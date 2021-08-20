27b66df8730ced68eda2ebae21ab46d64f2ec8a9
Helio Mesquita <helio.mesquitaios@gmail.com>
2021-08-19 21:05:54 -0300

    add folders and sample

---
+++ TestKrux/TestKrux/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TestKrux
+//
+//  Created by Hélio Mesquita on 19/08/21.
+//
+
+import UIKit
+import SDKKruxEncapsulation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        KruxTracker()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
